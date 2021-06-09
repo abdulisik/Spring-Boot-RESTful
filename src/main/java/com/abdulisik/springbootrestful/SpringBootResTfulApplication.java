1f184bb23d0a0119474c56c9fd3331ecfda44d75
Abdul Isik <sampiy10um@gmail.com>
2021-06-09 18:12:46 -0500

    Project initialization

---
+++ src/main/java/com/abdulisik/springbootrestful/SpringBootResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.abdulisik.springbootrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/abdulisik/springbootrestful/SpringBootResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulisik.springbootrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
